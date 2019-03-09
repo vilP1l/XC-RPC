8af4e6470532216f80cafaeba8f727339cd0f560
vilP1L <vilP1l@github.com>
2019-03-08 19:15:22 -0500

    add workspace detection

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import rpc from 'discord-rich-presence';
-import { execString } from 'applescript';
+import { execString, execFile } from 'applescript';
 
 const client = rpc('553717331273187338');
 const startTime = Date.now();
 const updateRPC = () => {
   execString('tell application "Xcode" to get the name of the front window', (err, res) => {
-    if (err) {
-      console.log(err);
-      process.exit();
-    }
-    client.updatePresence({
-      state: `Editing ${res}`,
-      startTimestamp: startTime,
-      largeImageKey: 'xcode',
-      largeImageText: 'Editing in XCode',
-      smallImageKey: res.includes('.swift') ? 'swift': 'unknown',
-      smallImageText: res.includes('.swift') ? 'Editing a .swift file': undefined,
-      instance: true,
+    execFile('src/getWorkspace.applescript', (err, project) => {
+      const workspace = project.replace('.xcodeproj', '');
+      client.updatePresence({
+        state: `Editing ${res}`,
+        details: `Working on ${workspace}`,
+        startTimestamp: startTime,
+        largeImageKey: 'xcode',
+        largeImageText: 'Editing in XCode',
+        smallImageKey: res.includes('.swift') ? 'swift': 'unknown',
+        smallImageText: res.includes('.swift') ? 'Editing a .swift file': undefined,
+        instance: true,
+      });
     });
   });
 }
