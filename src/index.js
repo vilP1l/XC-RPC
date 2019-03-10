332cade2ffd7867d290340ea2d0bf109241e1029
vilP1L <vilP1l@github.com>
2019-03-09 19:20:27 -0500

    some fixes

--- src/index.js
+++ src/index.js
@@ -14,27 +14,27 @@ console.log(`${'info'.green} Started RPC`);
 const updateRPC = () => {
   execString('tell application "System Events" to (name of processes) contains "Xcode"', (err, res) => {
     if (res === 'false') {
-      console.log(`${'err'.red} XCode is currently not open.`);
+      console.log(`${'err'.red} XCode is not open.`);
       process.exit();
     }
-  });
-  execString('tell application "Xcode" to get the name of the front window', (err, res) => {
-    execFile('src/getWorkspace.scpt', (err, project) => {
-      let workspace;
-      if (project) workspace = project.replace('.xcodeproj', '');
-      let fileExtension
-      if (res) fileExtension = res.match(/\.(.+)/g)[0];
-      const state = config.state.replace(/{file}/g, res);
-      const details = config.details.replace(/{project}/g, workspace);
-      client.updatePresence({
-        state: config.showState ? !res ? 'No file open' : state : undefined,
-        details: config.showDetails ? !workspace ? 'No project open' : details : undefined,
-        startTimestamp: startTime,
-        largeImageKey: 'xcode',
-        largeImageText: 'Editing in XCode',
-        smallImageKey: fileExtension ? fileExtension === '.swift' ? 'swift' : fileExtension === '.plist' ? 'plist' : 'unknown' : undefined,
-        smallImageText: fileExtension ? `Editing a ${fileExtension} file` : undefined,
-        instance: true,
+    execString('tell application "Xcode" to get the name of the front window', (err, res) => {
+      execFile('src/getWorkspace.scpt', (err, project) => {
+        let workspace;
+        if (project) workspace = project.replace('.xcodeproj', '');
+        let fileExtension
+        if (res) fileExtension = res.match(/\.(.+)/g)[0];
+        const state = config.state.replace(/{file}/g, res);
+        const details = config.details.replace(/{project}/g, workspace);
+        client.updatePresence({
+          state: config.showState ? !res ? 'No file open' : state : undefined,
+          details: config.showDetails ? !workspace ? 'No project open' : details : undefined,
+          startTimestamp: startTime,
+          largeImageKey: 'xcode',
+          largeImageText: 'Editing in XCode',
+          smallImageKey: fileExtension ? fileExtension === '.swift' ? 'swift' : fileExtension === '.plist' ? 'plist' : 'unknown' : undefined,
+          smallImageText: fileExtension ? `Editing a ${fileExtension} file` : undefined,
+          instance: true,
+        });
       });
     });
   });
